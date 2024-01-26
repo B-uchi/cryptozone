1204950313c7504277a63636b3862f3fd0130955
Ibuchukwu Umeano <ibumeano@gmail.com>
2024-01-26 22:03:08 +0100

    halfway through searching on coin page

--- src/pages/Cryptocurrencies.tsx
+++ src/pages/Cryptocurrencies.tsx
@@ -1,10 +1,25 @@
+import { useState } from "react";
 import CoinGrid from "../components/CoinGrid";
 import { useGetCryptocurrenciesQuery } from "../services/cryptocurrencyAPI";
+import { IoMdSearch } from "react-icons/io";
+import { HeroSectionProps } from "../components/HeroSection";
 
 const Cryptocurrencies = () => {
   const { data, error, isLoading } = useGetCryptocurrenciesQuery({});
+  const [searchParam, setSearchParam] = useState("");
   const coins = data?.data?.coins;
-  console.log(coins)
+  let filteredCoins = coins;
+
+  type CoinType = {
+    coin: HeroSectionProps["coin"];
+  };
+
+  const searchCoins = (searchParam: string) => {
+    filteredCoins = coins.filter((coin: CoinType["coin"]) => {
+      return coin.name.toLowerCase().includes(searchParam.toLowerCase());
+    });
+  };
+  console.log(searchParam, filteredCoins)
   return (
     <div className="md:h-[100vh] flex justify-center  w-full text-black dark:text-white">
       {isLoading ? (
@@ -16,8 +31,29 @@ const Cryptocurrencies = () => {
         </div>
       ) : (
         <div className="w-[90%] p-2 mt-3">
-          <h1 className="text-3xl mb-5 font-bold">Cryptocurrencies</h1>
-          <CoinGrid coins={coins} />
+          <div
+            className="flex justify-evenly items-baseline w-full
+          "
+          >
+            <h1 className="text-2xl mb-5 font-bold">Cryptocurrencies</h1>
+            <div className="border-[1px] w-[200px] border-[#d3d3d3] dark:border-[#9e9e9e] p-2 rounded-full text-[12px] text-[#d3d3d3] flex items-center gap-3">
+              <button
+                type="button"
+                title="search-btn"
+                onClick={() => searchCoins(searchParam)}
+              >
+                <IoMdSearch size={25} />
+              </button>
+              <input
+                type="text"
+                className="w-[90%] bg-transparent outline-none"
+                placeholder="Type to search..."
+                onChange={(e) => setSearchParam(e.target.value)}
+                value={searchParam}
+              />
+            </div>
+          </div>
+          <CoinGrid coins={filteredCoins} />
         </div>
       )}
     </div>
