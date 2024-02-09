98a3dcbb7d4697c96f3f34ffb663041a2887b9d9
Ibuchukwu Umeano <ibumeano@gmail.com>
2024-02-09 20:44:56 +0100

    ADD: Created exchanges page

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,21 @@ import HomePage from "./pages/HomePage";
 import Cryptocurrencies from "./pages/Cryptocurrencies";
 import News from "./pages/News";
 import CoinDetails from "./pages/CoinDetails";
+import Exchanges from "./pages/Exchanges";
 
 function App() {
   return (
     <Router>
       <div className=" h-fit w-full">
         <Navbar />
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/coins" element={<Cryptocurrencies/>} />
-            <Route path="/news" element={<News/>} />
-            <Route path="*" element={<h1>404 Not Found</h1>} />
-            <Route path="/coins/:uuid" element={<CoinDetails/>} />
-          </Routes>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/coins" element={<Cryptocurrencies />} />
+          <Route path="/news" element={<News />} />
+          <Route path="*" element={<h1>404 Not Found</h1>} />
+          <Route path="/coins/:uuid" element={<CoinDetails />} />
+          <Route path="/exchanges" element={<Exchanges />} />
+        </Routes>
       </div>
     </Router>
   );
